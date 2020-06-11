94804df2656e80babdf9d0cbb35535609c7f54c1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-11 15:02:02 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,8 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend FancyDance::Instance Methods
-  include FancyDance::ClassMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
    
   attr_accessor :name
  
